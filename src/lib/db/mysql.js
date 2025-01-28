cf85c9d74230430e3f125636517e0d433070a3c4
Ermi <ermhut19@htl-shkoder.com>
2025-01-28 12:29:45 +0100

    Added .env file

--- src/lib/db/mysql.js
+++ src/lib/db/mysql.js
@@ -1,14 +1,21 @@
 import mysql from 'mysql2/promise';
+import {
+	DB_HOST,
+	DB_USER,
+	DB_PORT,
+	DB_PASSWORD,
+	DB_NAME
+} from '$env/static/private';
 
 let connection = null;
 export function createConnection() {
 	if (!connection) {
 		connection = mysql.createConnection({
-			host: 'htl-projekt.com',
-			user: 'ermihuti',
-			port: 33060,
-			password: '!Insy_2024$',
-			database: '2024_4bw_ermihuti_event_app'
+			host: DB_HOST,
+			user: DB_USER,
+			port: DB_PORT,
+			password: DB_PASSWORD,
+			database: DB_NAME
 		});
 	}
 	return connection;
